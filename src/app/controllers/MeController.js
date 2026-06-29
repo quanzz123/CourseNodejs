ee234777ceb00f85896339db06841e4c6429654c
quanzz123 <nguyenquandc37@gmail>
2026-06-29 22:37:22 +0700

    resore course

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -6,7 +6,19 @@ import {mongooseToObject, multipleMongooseToObject} from '../../util/mongoose.js
 class MeController {
     //[GET] /mes/trash/show
     showTrashCourse(req, res, next) {
-        
+        Course.findDeleted({})
+            .then(courses => {
+                res.render('me/showTrashCourses', {
+                    courses: multipleMongooseToObject(courses)
+                })
+            })
+            .catch(next)
+    }
+    //[PATCH] mes/:id/restore
+    restore(req, res, next) {
+        Course.restore({_id: req.params.id})
+            .then(() => res.redirect('/mes/trash/show'))
+            .catch(next)
     }
 }
 
--- src/routes/me.js
+++ src/routes/me.js
@@ -4,5 +4,6 @@ import MeController from '../app/controllers/MeController.js';
 const router = express.Router();
 
 router.get('/trash/show', MeController.showTrashCourse);
+router.patch('/:id/restore', MeController.restore)
 
 export default router;
